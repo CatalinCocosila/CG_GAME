d33f31a72f7455569d4efd3bb6540dd37ee67d8a
MihaiRuta <ruta.mihai.k2y@student.ucv.ro>
2022-05-17 13:19:16 +0300

    player input

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    private const float MAX_LEFT = -2.5f;
+    private const float MAX_RIGHT = 2.5f;
+
+    private float speed = 3;
+    private float cooldown = 0.5f;
+
+    private bool isShooting;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKey(KeyCode.A) && transform.position.x > MAX_LEFT)
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+
+        if (Input.GetKey(KeyCode.D) && transform.position.x < MAX_RIGHT)
+            transform.Translate(Vector2.right * Time.deltaTime * speed);
+
+        if (Input.GetKey(KeyCode.Space) && !isShooting)
+            StartCoroutine(Shoot());
+#endif
+    }
+
+    private IEnumerator Shoot()
+    {
+        isShooting = true;
+        Instantiate(bulletPrefab, transform.position, Quaterntion.identity);
+        yeld return new WaitForSeconds(cooldown);
+        isShooting = false;
+    }
+}
